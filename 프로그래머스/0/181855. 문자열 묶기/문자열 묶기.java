e954158dd2f252afd39bed7e5e4b208248072238
soos <92793904+MirrorPI@users.noreply.github.com>
2026-08-29 01:27:19 +0900

    [level 0] Title: 문자열 묶기, Time: 5.03 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int[] count = new int[31];
+        for(String str: strArr) {
+            count[str.length()]++;
+        }
+        int answer = 0;
+        for(int i=0; i<31; i++) {
+            answer = Math.max(answer, count[i]);
+        }
+        
+        return answer;
+    }
+}
